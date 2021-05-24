95c116049106badaa502d269708401863af6088a
lindseyreynolds44 <56812063+lindseyreynolds44@users.noreply.github.com>
2021-05-24 11:02:37 -0700

    First commit

---
+++ cst438-project-airline/src/main/java/cst438/Cst438ProjectAirlineApplication.java
@@ -0,0 +1,13 @@
+package cst438;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cst438ProjectAirlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cst438ProjectAirlineApplication.class, args);
+	}
+
+}
